8daa7f82fda75dac6fee45e980b74530a946236e
LucasMique <20261ctb0100030@estudantes.ifpr.edu.br>
2026-04-07 17:06:27 +0000

    Aula 10 - 1000: Hello World

---
+++ beecrowd/1000-hello-world.py
@@ -0,0 +1,16 @@
+'''
+
+Problema: beecrowd | 1000
+Data: 2026.04.07
+Estudante: Lucas Klipan Miquelin
+'''
+# Objetivo: Exibir exatamente a mensagem "Hello World!" na tela
+
+# --- ANÁLISE (LIAC)
+# ENTRADA: nenhuma, esse problema nao requer leitura de dados
+# Processamento: nenhum, não há cálculo ou transformação
+# Saída: exibir o texto "Hello World!" exatamente como especificado
+
+# print() é o comando de saída em Python
+#Equivalente ao bloco "Diga ..." do Scratch
+print('Hello World!')
