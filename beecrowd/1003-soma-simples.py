e006a9f724de71cc8468adf4724ec4f2c61c4238
LucasMique <20261ctb0100030@estudantes.ifpr.edu.br>
2026-04-07 17:34:56 +0000

    Aula 10 - 1003: soma simples

---
+++ beecrowd/1003-soma-simples.py
@@ -0,0 +1,21 @@
+'''
+problema: beecrowd | 1003
+Data: 2026.04.07
+Estudante: Lucas Klipan Miquelin
+'''
+# Objetivo: Somar dois valores e ter o output certo
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: Dois inputs feitos pelo beecrowd/eu, A e B.
+# Processamento: cálculos feitos pelo computador para somar A e B.
+# Saída: A soma de A e B
+
+# Leitura das variáveis (Ovserve o enunciado - quantas entradas há?)
+A = int(input())
+B = int(input())
+
+# Cálculo
+SOMA = A + B
+
+# Saída - Observe o formato exato no enunciado
+print(f"SOMA = {SOMA}")
