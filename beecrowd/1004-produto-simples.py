f1efe6e7c84729c3783e77d5abd4d0ef1b7c7dc0
LucasMique <20261ctb0100030@estudantes.ifpr.edu.br>
2026-04-07 17:41:05 +0000

    Aula 10 - 1004: produto simples

---
+++ beecrowd/1004-produto-simples.py
@@ -0,0 +1,20 @@
+'''
+problema: beecrowd | 1004
+Data: 2026.04.07
+Estudante: Lucas Klipan Miquelin
+'''
+# Objetivo: Multiplicar A e B, tendo o produto listado como "PROD = [...]"
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: Dois inputs, A e B.
+# Processamento: Cálculo de multiplicação entre A e B.
+# Saída: Produto final dessa multiplicação, com "PROD = " antes.
+
+A = int(input())
+B = int(input())
+
+# Qual operação o enunciado pede?
+PROD = A * B
+
+# Saída - observe o formato exato no enunciado
+print(f"PROD = {PROD}")
