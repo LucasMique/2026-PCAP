39e1bda0246a0516827e68065a7ffdc34c91bfea
LucasMique <20261ctb0100030@estudantes.ifpr.edu.br>
2026-04-09 20:33:46 +0000

    Aula 11 - 1019: conversao de tempo

--- beecrowd/1019-conversao-de-tempo.py
+++ beecrowd/1019-conversao-de-tempo.py
@@ -6,3 +6,28 @@ estudante: Lucas Klipan Miquelin
 # Objetivo: ler uma duração em segundos e convertê-la para Horas:minutos:segundos
 
 # --- Análise (liac) ---
+# Entrada: um numero N inteiro representando segundos totais
+# processamento: extrair horas. minutos e segundos restantes por divisão inteira e módulo
+# saída: no formato h:m:s (sem zeros á esquerda - 0:9:16, não 00:09:16)
+
+#int(input()) duração sempre é um numero inteiro de segundos
+N = int(input())
+
+# // divisão inteira: retorna quantas vezes o divisor cabe no dividendo
+# %  módulo: retorna apenas o segundo da divisão
+
+#quantas horas completas cabem em N segundos? (1 hora = 3600 segundos)
+h = N // 3600
+
+#segundos restantes após retirar as horas completas
+N = N % 3600
+
+# quantos minutos completos cabem nos segundos restantes? (1 min igual 60 segundos)
+m = N // 60
+
+# segundos que sobram após retirar os minutos completos
+s = N % 60
+
+# f-string monta o formato h:m:s - sem zeros á esquerda
+
+print(f"{h}:{m}:{s}")'''
