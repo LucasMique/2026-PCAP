82724d7c1af46a0b13794923ef023a8ede07cc5e
LucasMique <20261ctb0100030@estudantes.ifpr.edu.br>
2026-04-12 04:23:33 +0000

    Aula 11 - 1020: idade em dias

---
+++ beecrowd/1020-idade-em-dias.py
@@ -0,0 +1,33 @@
+'''
+problema: beecrowd | 1020
+data: 2026.04.09
+estudante: Lucas Klipan Miquelin
+'''
+# Objetivo: ler dias e converter em ano, meses, e dias
+
+# --- Análise (liac) ---
+# Entrada: um número D inteiro lendo dias
+# processamento: pegar dias, meses
+# saída: no formato h:m:s (sem zeros á esquerda - 0:9:16, não 00:09:16)
+
+#int(input()) duração sempre é um numero inteiro de segundos
+N = int(input())
+
+# // divisão inteira: retorna quantas vezes o divisor cabe no dividendo
+# %  módulo: retorna apenas o segundo da divisão
+
+#quantas horas completas cabem em N segundos? (1 hora = 3600 segundos)
+h = N // 3600
+
+#segundos restantes após retirar as horas completas
+N = N % 3600
+
+# quantos minutos completos cabem nos segundos restantes? (1 min igual 60 segundos)
+m = N // 60
+
+# segundos que sobram após retirar os minutos completos
+s = N % 60
+
+# f-string monta o formato h:m:s - sem zeros á esquerda
+
+print(f"{h}:{m}:{s}")
