6bad0102541ffe22c993fd313690374a6970d8ba
LucasMique <20261ctb0100030@estudantes.ifpr.edu.br>
2026-04-23 20:00:51 +0000

    Aula 13: distância

---
+++ beecrowd/1016-distancia.py
@@ -0,0 +1,23 @@
+'''
+problema | beecrowd 1016
+data: 2026.04.23
+estudante: lucas klipan miquelin
+'''
+
+#objetivo: calcular o quanto de tempo um carro se afasta do outro, tendo em conta que é 1km a cada 2minutos
+
+# --- análise (liac) ---
+
+# entrada: distância em km
+# processamento: professor berssa esse exercício é EXTREMAMENTE fácil já que o único processamento necessário é multiplicar a entrada por 2
+# saída: resultado e minutos ao lado
+
+# entradaxx
+A = int(input())
+
+# processamentoKKKKKKK
+B = A * 2
+
+# saída
+
+print(f"{B} minutos")
