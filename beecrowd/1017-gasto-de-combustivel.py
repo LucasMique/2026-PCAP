edeb92ecaf6d66861c052b5dd4181be1b6631ae0
LucasMique <20261ctb0100030@estudantes.ifpr.edu.br>
2026-04-23 19:31:27 +0000

    Aula 13: gasto de combustível

---
+++ beecrowd/1017-gasto-de-combustivel.py
@@ -0,0 +1,28 @@
+'''
+problema: beecrowd | 1017
+data: 2026.04.23
+estudante: lucas klipan miquelin
+'''
+
+# Objetivo: calcular a quantidade de litros gasto em uma viagem
+
+# --- Análise (Liac) ---
+#entrada: dois valores, tempo gasto na viagem (h) e a velocidade média durante a mesma.
+#processamento: calculo entre ambos
+#saída: quantidade de litros necessária
+
+# inputzzz
+T = int(input())
+V = int(input())
+
+# procesamentoxxx nao chroraxx
+
+M = T * V
+
+#resultadoxxxx
+R = M / 12
+
+# saídaxxxx
+
+print(f"{R:.3f}")
+
