14cd7958c240d317c19bd758df61e0578d89f001
LucasMique <20261ctb0100030@estudantes.ifpr.edu.br>
2026-06-09 16:30:17 +0000

    aula16: jogo adivinhe o numero com 3 niveis

--- aula16/adivinhe.py
+++ aula16/adivinhe.py
@@ -8,6 +8,20 @@
 
 import random
 
+
+# Esses são os códigos dos segredos. caso usados, irão fazer uma mensagem especial. Ou, até, um cheat para você ganhar imediatamente.
+baaa = 1000
+
+resenhéx =  67
+
+numerofavorito = 777
+
+aresenhanaopara = 999
+
+nope = 666
+
+ganhei = 676767077707
+
 # === Sub-rotina: o jogo inteiro vira uma função reutilizável ===
 def jogar(maximo, chances):
     numero_secreto = random.randint(1, maximo)
@@ -18,6 +32,19 @@ def jogar(maximo, chances):
 
         if palpite == numero_secreto:
             print("🎊 Acertou!")
+            acertou = True
+        elif palpite == resenhéx:
+            print("bandido quer 67 resenha bandido quer 67 resenha")
+        elif palpite == nope:
+            print("👹")
+        elif palpite == numerofavorito:
+            print("haha esse é meu número favorito! é perfeito.")
+        elif palpite == aresenhanaopara:
+            print("esse número é maligno. tão perto de 1000 mas tão longe....")
+        elif palpite == baaa:
+            print("Essa é a tropa das ovelhas! Aqui só há ovelhas e resenha. 🐑🐑🐑🐑 Bááááá 🐑🐑🐑🐑")
+        elif palpite == ganhei:
+            print("Oloko seu xiter horrível ganhou xitando lirili larilá orcalero orcalá tung tung tung sahurr")
             acertou = True
         elif palpite < numero_secreto:
             print("📈 Muito baixo!")
@@ -41,6 +68,7 @@ print(" Escolha o nível de difficuldade:")
 print("1 - Fácil (1 a 10, 3 chances)")
 print("2 - Médio (1 a 100, 5 chances)")
 print("3, Impossível (1 a 1000, 10 chances)")
+print("Você consegue encontrar todos os segredos nesse jogo?")
 opcao = int(input("Digite 1, 2, ou 3:"))
 
 # A opção 1 está na posição 0 da lista, por isso o ajuste
