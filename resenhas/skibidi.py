2987ecb88974af748dcb3ee1d2180e5802aed1ee
LucasMique <20261ctb0100030@estudantes.ifpr.edu.br>
2026-06-20 00:00:20 +0000

    Resenha n4: Programa autoritário de contagem.

---
+++ resenhas/skibidi.py
@@ -0,0 +1,9 @@
+# Esse é um arquivo de teste para um programa autoritário. Sugerido por meu amigo após ele me mandar um texto contendo "1 skibidi toilet, 2 skibidi toilet, [...]" achei legal replicar esse texto em forma de código.
+
+numero = int(input("Escolha até quando vai a contagem: "))
+numero = numero + 1
+
+x = input("Escolha o que será contado: ")
+
+for numero in range(1, numero):
+    print(f"{numero} {x}")
